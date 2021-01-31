4884f9a76d09273dec9cd3916a31a57a7edfbd98
Andrew Tan <andrewtan367@gmail.com>
2021-01-31 12:45:36 +0800

    Finishing Slicing Details Page

--- src/js/main.js
+++ src/js/main.js
@@ -248,3 +248,37 @@ import { addClass, removeClass } from "./utils-class";
     window.addEventListener("load", accordion);
   }
 })();
+
+// ---------------------------------------- Slider Details Section------------------------------------
+(() => {
+  const sliders = document.getElementsByClassName("slider");
+
+  for (let index = 0; index < sliders.length; index++) {
+    const slider = sliders[index];
+
+    const items = slider.querySelectorAll(".slider .item");
+
+    const preview = slider.querySelector("div > .preview");
+
+    for (let item = 0; item < items.length; item++) {
+      const itemTrigger = items[item];
+
+      itemTrigger.addEventListener("click", function () {
+        const dataImg = this.attributes?.["data-img"]?.value;
+
+        for (
+          let eachItemTrigger = 0;
+          eachItemTrigger < items.length;
+          eachItemTrigger++
+        ) {
+          const triggerNeedToRemoved = items[eachItemTrigger];
+
+          removeClass(triggerNeedToRemoved, "selected");
+        }
+
+        addClass(itemTrigger, "selected");
+        preview.querySelector("img").setAttribute("src", dataImg);
+      });
+    }
+  }
+})();
